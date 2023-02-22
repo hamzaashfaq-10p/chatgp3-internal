bc9732818bf16b9bf5506d2baaa46eedf41fc6ae
Hamza Ashfaq <hamza.ashfaq@10pearls.com>
2023-02-22 22:28:08 +0500

    Changes

--- app.js
+++ app.js
@@ -25,70 +25,100 @@ app.get("/", function (req, res) {
 });
 
 let fileContent = "";
+let readFileSync = null;
+let data = undefined;
+
+const getData = async (readFileSync) => {
+  return new Promise(function (myResolve, myReject) {
+    pdfExtractParser.extractBuffer(
+      readFileSync,
+      { normalizeWhitespace: true, disableCombineTextItems: true },
+      (err, data) => {
+        if (err) {
+          myReject(err);
+        }
+        myResolve(data);
+      }
+    );
+  });
+};
 
 app.post("/upload", upload.single("file"), async (req, resp) => {
   // Read the uploaded file as text
   var answer = "";
-  let readFileSync;
+  var finalAnswer = "";
+
+  let waiting = true;
 
   try {
     if (req.file?.path) {
-      readFileSync = fs.readFileSync(req.file.path);
-      pdfExtractParser.extractBuffer(
-        readFileSync,
-        { normalizeWhitespace: true, disableCombineTextItems: true },
-        async (err, data) => {
-          if (err) {
-            resp.send({ message: err });
-            return;
-          }
+      if (!readFileSync && !data) {
+        console.log("Came here");
+        readFileSync = fs.readFileSync(req.file.path);
+        data = await getData(readFileSync);
+      }
+      const res = [];
+      for (let i = 0; i < data.pages.length; i += 8) {
+        const chunk = data.pages.slice(i, i + 8);
+        res.push(chunk);
+      }
 
-          const res = [];
-          for (let i = 0; i < data.pages.length; i += 5) {
-            const chunk = data.pages.slice(i, i + 5);
-            res.push(chunk);
+      for (let i = 0; i < res.length; i++) {
+        for (let j = 0; j < res[i].length; j++) {
+          for (let k = 0; k < res[i][j].content.length; k++) {
+            answer += res[i][j].content[k].str;
           }
-
-          for (let i = 0; i < res.length; i++) {
-            answer = "";
-            console.log(i);
-            for (let j = 0; j < res[i].length; j++) {
-              for (let k = 0; k < res[i][j].content.length; k++) {
-                answer += res[i][j].content[k].str;
-              }
-            }
-
-            fileContent = answer;
-            answer = await runCompletion(fileContent, req.body.question);
-
-            if (
-              (answer && answer.trim() === "I don't know") ||
-              answer.trim() === "I don't know."
-            ) {
-              continue;
-            } else {
-              break;
-            }
-          }
-          if (
-            (answer && answer.trim() === "I don't know") ||
-            answer.trim() === "I don't know."
-          ) {
-            let newAnswer = await runCompletionFromModel(req.body.question);
-            answer = `The document didn't contain the required answer so this is what i got from my model : \n ${newAnswer}`;
-          }
-          resp.send({ message: answer });
         }
-      );
+
+        if (!waiting) {
+          break;
+        }
+
+        fileContent = answer;
+        answer = await runCompletion(fileContent, req.body.question);
+        console.log("Batch", i);
+        if (
+          (answer && answer.trim() !== "I don't know") ||
+          answer.trim() !== "I don't know."
+        ) {
+          finalAnswer += answer;
+        }
+      }
+
+      if (
+        (answer && answer.trim() === "I don't know") ||
+        answer.trim() === "I don't know."
+      ) {
+        let newAnswer = await runCompletionFromModel(req.body.question);
+        answer = `The document didn't contain the required answer so this is what i got from my model : \n ${newAnswer}`;
+      }
+
+      if (waiting) {
+        resp.send({ message: answer });
+        waiting = false;
+        return;
+      }
     } else {
       let newAnswer = await runCompletionFromModel(req.body.question);
       answer = `The document didn't contain the required answer so this is what i got from my model : \n ${newAnswer}`;
-
-      resp.send({ message: answer });
+      if (waiting) {
+        resp.send({ message: answer });
+        waiting = false;
+        return;
+      }
     }
   } catch (error) {
     resp.send({ message: error });
   }
+
+  setTimeout(() => {
+    if (waiting) {
+      waiting = false;
+      console.log("Answer sent");
+      resp.send({ message: finalAnswer });
+      return;
+    }
+  }, 31000);
 });
 
 app.post("/searchModel", upload.single("file"), async (req, res) => {
@@ -102,7 +132,7 @@ app.post("/searchModel", upload.single("file"), async (req, res) => {
 
 async function runCompletion(text, question) {
   const completion = await openai.createCompletion({
-    max_tokens: 500,
+    max_tokens: 200,
     model: "text-davinci-003",
     prompt: `Answer the question as completely in detail using the provided document, and if the answer is not contained within the document , and if you cannot find any answer say "I don't know"
   
