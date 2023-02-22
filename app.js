5f7ce59fe035d50adf8d0bc2c439d5f4c6549de9
Hamza Ashfaq <hamza.ashfaq@10pearls.com>
2023-02-22 12:59:16 +0500

    Changes

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var path = require("path");
 var bodyParser = require("body-parser");
 const multer = require("multer");
 const fs = require("fs");
-const pdfParse = require("pdf-parse");
+const PDFExtract = require("pdf.js-extract").PDFExtract;
 const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
+const pdfExtractParser = new PDFExtract();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -25,7 +26,7 @@ app.get("/", function (req, res) {
 
 let fileContent = "";
 
-app.post("/upload", upload.single("file"), async (req, res) => {
+app.post("/upload", upload.single("file"), async (req, resp) => {
   // Read the uploaded file as text
   var answer = "";
   let readFileSync;
@@ -33,22 +34,55 @@ app.post("/upload", upload.single("file"), async (req, res) => {
   try {
     if (req.file?.path) {
       readFileSync = fs.readFileSync(req.file.path);
-      let pdfExtract = await pdfParse(readFileSync);
-      fileContent = pdfExtract.text;
+      pdfExtractParser.extractBuffer(
+        readFileSync,
+        { normalizeWhitespace: true, disableCombineTextItems: true },
+        async (err, data) => {
+          if (err) {
+            resp.send({ message: err });
+            return;
+          }
+
+          const res = [];
+          for (let i = 0; i < data.pages.length; i += 5) {
+            const chunk = data.pages.slice(i, i + 5);
+            res.push(chunk);
+          }
+
+          for (let i = 0; i < res.length; i++) {
+            answer = "";
+            console.log(i);
+            for (let j = 0; j < res[i].length; j++) {
+              for (let k = 0; k < res[i][j].content.length; k++) {
+                answer += res[i][j].content[k].str;
+              }
+            }
+
+            fileContent = answer;
+            answer = await runCompletion(fileContent, req.body.question);
+
+            if (
+              (answer && answer.trim() === "I don't know") ||
+              answer.trim() === "I don't know."
+            ) {
+              continue;
+            } else {
+              break;
+            }
+          }
+          if (
+            (answer && answer.trim() === "I don't know") ||
+            answer.trim() === "I don't know."
+          ) {
+            let newAnswer = await runCompletionFromModel(req.body.question);
+            answer = `The document didn't contain the required answer so this is what i got from my model : \n ${newAnswer}`;
+          }
+          resp.send({ message: answer });
+        }
+      );
     }
-    console.log(fileContent);
-    answer = await runCompletion(fileContent, req.body.question);
-    console.log(answer);
-    if (
-      (answer && answer.trim() === "I don't know") ||
-      answer.trim() === "I don't know."
-    ) {
-      let newAnswer = await runCompletionFromModel(req.body.question);
-      answer = `The document didn't contain the required answer so this is what i got from my model : \n ${newAnswer}`;
-    }
-    res.send({ message: answer });
   } catch (error) {
-    res.send({ message: error });
+    resp.send({ message: error });
   }
 });
 
